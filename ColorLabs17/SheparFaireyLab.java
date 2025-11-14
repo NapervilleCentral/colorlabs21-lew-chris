abe2b3977963b7f689701723a2388c07a191a1f3
lew-chris <chlew@stu.naperville203.org>
2025-11-14 11:25:56 -0600

    done

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -30,7 +30,9 @@ public class SheparFaireyLab
          Picture me1 = new Picture("images/cubs.jpg");
          Picture me2 = new Picture("images/cubs.jpg");
          Picture me3 = new Picture("images/cubs.jpg");
-
+         Picture me4 = new Picture("images/cubs.jpg");
+         Picture me5 = new Picture("images/cubs.jpg");
+         
          Pixel[] M1pixels;
          M1pixels = me1.getPixels();
          
@@ -39,6 +41,12 @@ public class SheparFaireyLab
          
          Pixel[] M3pixels;
          M3pixels = me3.getPixels();
+         
+         Pixel[] M4pixels;
+         M4pixels = me4.getPixels();
+
+         Pixel[] M5pixels;
+         M5pixels = me5.getPixels();
          
          /**
           * method 1 change
@@ -78,7 +86,9 @@ public class SheparFaireyLab
          /**
           * method 2 change
           */
-         int max = -1, min = 300, diff;
+         
+         
+         int max = -1, min = 300, diff=0;
          for (Pixel p : M2pixels)
          {
             avg = (int)(p.getAverage());
@@ -104,14 +114,13 @@ public class SheparFaireyLab
                 p.setColor(offwhite);
          }
          me2.explore();
+         
          /**
           * custom color palette
           */
          
-         Color offwhite = new Color(230, 220, 199);
-         Color lightblue = new Color(106, 174, 199);
-         Color darkblue = new Color(24, 67, 83);
-         Color newred = new Color(166, 15, 15);
+         Color orange = new Color(255, 159, 64);
+         Color gray = new Color(211,211,211);
          
          for (Pixel p : M3pixels)
          {
@@ -120,22 +129,68 @@ public class SheparFaireyLab
             p.setGreen(avg);
             p.setRed(avg);
          }
-         me3.explore();
-         
          for (Pixel p : M3pixels)
          {  
             blue = p.getBlue();
-            if (blue < 64)
+            if (blue < 70)
                 p.setColor(darkblue);
             else if (blue < 126)
-                p.setColor(newred);
+                p.setColor(orange);
             else if (blue < 189)
                 p.setColor(lightblue);
             else
                 p.setColor(offwhite);
          }
          me3.explore();
-         me3.write("images/ShepardFairey1.jpg");
-
+         me3.write("images/SFtry1.jpg");
+         
+         for (Pixel p : M4pixels)
+         {
+            avg = (int)(p.getAverage());
+            p.setBlue(avg);
+            p.setGreen(avg);
+            p.setRed(avg);
+         }
+         for (Pixel p : M4pixels)
+         { 
+            blue = p.getBlue();
+            if (blue < 70)
+                p.setColor(darkblue);
+            else if (blue < 110)
+                p.setColor(lightblue);
+            else if (blue < 165)
+                p.setColor(orange);
+            else if (blue < 200)
+                p.setColor(gray);
+            else
+                p.setColor(offwhite);
+         }
+         me4.explore();
+         me4.write("images/SFtry2.jpg");
+         
+         for (Pixel p : M5pixels)
+         {
+            avg = (int)(p.getAverage());
+            p.setBlue(avg);
+            p.setGreen(avg);
+            p.setRed(avg);
+         }
+         for (Pixel p : M5pixels)
+         { 
+            blue = p.getBlue();
+            if (blue < 70)
+                p.setColor(darkblue);
+            else if (blue < 120)
+                p.setColor(lightblue);
+            else if (blue < 165)
+                p.setColor(orange);
+            else if (blue < 190)
+                p.setColor(gray);
+            else
+                p.setColor(offwhite);
+         }
+         me5.explore();
+         me5.write("images/SFtry3.jpg");
+         
     }//main       
 }//class
